c53edeafff53e3ebd34f1cab0d8bf8138f752a55
ragor114 <69634980+ragor114@users.noreply.github.com>
2021-12-20 17:20:57 +0100

    added starter

---
+++ src/SpeedDial/SpeedDialApp.java
@@ -0,0 +1,19 @@
+package SpeedDial;
+
+import de.ur.mi.oop.app.GraphicsApp;
+import de.ur.mi.oop.launcher.GraphicsAppLauncher;
+
+public class SpeedDialApp extends GraphicsApp {
+    public void initialize() {
+
+    }
+
+    @Override
+    public void draw() {
+
+    }
+
+    public static void main(String[] args) {
+        GraphicsAppLauncher.launch();
+    }
+}
